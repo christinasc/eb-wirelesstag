88a720a6364206fb464cc7ed94dd226a7544ba86
Christina <christinasc@gmail.com>
2017-01-25 22:16:35 -0800

    add links, cron on 1st of month

--- goldlab.php
+++ goldlab.php
@@ -36,20 +36,43 @@ else
         <h2>Current Status</h2>
         <ul>
 
-
-
     <li><a href="https://my.wirelesstag.net/eth/tempStats.html?39ba7d42-bab8-40ba-aaa8-68baca0fc996&GLD-Fridge%201&C"> Gold Lab Fridge #1- View Graph </a>
 
 <iframe src="https://my.wirelesstag.net/ethSharedFrame.aspx?pic=1&hide_name=0&hide_temp=0&hide_rh=0&hide_updated=0&hide_signal=1&hide_battery=0&hide_battery_volt=1&hide_motion=1&uuids=39ba7d42-bab8-40ba-aaa8-68baca0fc996" height="62" width="100%" frameborder="0" scrolling="no"></iframe>
-
     </li>
-
     
-    <li> <a href="https://my.wirelesstag.net/eth/tempStats.html?1fa24424-4b86-464c-bcfc-a873d3d418cb&GLD-Freezer%201&C"> Gold Lab Fridge #2- View Graph</a>
+    <li> <a href="https://my.wirelesstag.net/eth/tempStats.html?1fa24424-4b86-464c-bcfc-a873d3d418cb&GLD-Freezer%201&C"> Gold Lab Freezer #1- View Graph</a>
 
 <iframe src="https://my.wirelesstag.net/ethSharedFrame.aspx?pic=1&hide_name=0&hide_temp=0&hide_rh=0&hide_updated=0&hide_signal=1&hide_battery=0&hide_battery_volt=1&hide_motion=1&uuids=1fa24424-4b86-464c-bcfc-a873d3d418cb" height="62" width="100%" frameborder="0" scrolling="no"></iframe>
 
     </li>
+
+ <li> <a href="https://my.wirelesstag.net/eth/tempStats.html?b4b0eb21-53e6-44da-939b-282c133d4934&GLD-Fridge%202&C"> Gold Lab Fridge #2- View Graph</a>
+
+<iframe src="https://my.wirelesstag.net/ethSharedFrame.aspx?pic=1&hide_name=0&hide_temp=0&hide_rh=0&hide_updated=0&hide_signal=1&hide_battery=0&hide_battery_volt=1&hide_motion=1&uuids=b4b0eb21-53e6-44da-939b-282c133d4934" height="62" width="100%" frameborder="0" scrolling="no"></iframe>
+    </li>
+
+
+ <li> <a href="https://my.wirelesstag.net/eth/tempStats.html?ef75faca-5eb3-497a-b3bf-9b83513f51a0&GLD-Freezer%202&C"> Gold Lab Freezer #2- View Graph</a>
+
+<iframe src="https://my.wirelesstag.net/ethSharedFrame.aspx?pic=1&hide_name=0&hide_temp=0&hide_rh=0&hide_updated=0&hide_signal=1&hide_battery=0&hide_battery_volt=1&hide_motion=1&uuids=ef75faca-5eb3-497a-b3bf-9b83513f51a0" height="62" width="100%" frameborder="0" scrolling="no"></iframe>
+
+    </li>
+
+ <li> <a href="https://my.wirelesstag.net/eth/tempStats.html?e9fd47c8-b3a8-414f-9b5d-f4869c8f85db&GLD-Fridge%203&C"> Gold Lab Fridge #3- View Graph</a>
+
+<iframe src="https://my.wirelesstag.net/ethSharedFrame.aspx?pic=1&hide_name=0&hide_temp=0&hide_rh=0&hide_updated=0&hide_signal=1&hide_battery=0&hide_battery_volt=1&hide_motion=1&uuids=e9fd47c8-b3a8-414f-9b5d-f4869c8f85db" height="62" width="100%" frameborder="0" scrolling="no"></iframe>
+    </li>
+
+
+ <li> <a href="https://my.wirelesstag.net/eth/tempStats.html?c59d819c-50eb-4b9a-8bcf-feb5fb349fcb&GLD-Freezer%203&C"> Gold Lab Freezer #3- View Graph</a>
+ 
+<iframe src="https://my.wirelesstag.net/ethSharedFrame.aspx?pic=1&hide_name=0&hide_temp=0&hide_rh=0&hide_updated=0&hide_signal=1&hide_battery=0&hide_battery_volt=1&hide_motion=1&uuids=c59d819c-50eb-4b9a-8bcf-feb5fb349fcb" height="62" width="100%" frameborder="0" scrolling="no"></iframe>
+
+    </li>
+
+
+
 
         </ul>
 
